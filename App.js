5ba087dcee98879b3d3b8fd8921ff86982bc233a
Kanit Sharma <kanitshrm09@gmail.com>
2017-08-20 20:32:10 +0530

    Progress

--- App.js
+++ App.js
@@ -1,26 +1,10 @@
-/* eslint no-use-before-define: 0 */
-/* eslint react/prefer-stateless-function : 0 */
+import React from 'react'
+import Main from './pages/main'
+import { Provider } from 'react-redux'
+import store from './store'
 
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+const App = () => {
+  <Provider store={store}>
+    <Main />
+  </Provider>
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles/main.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+export defaults styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
