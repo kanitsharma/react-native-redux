d7d9ba3284aa4b24f2e1f3c9fe5a822a84d9443f
Kanit Sharma <kanitshrm09@gmail.com>
2017-08-22 19:44:10 +0530

    Prototype finished

---
+++ store/actions.js
@@ -0,0 +1,5 @@
+export const TYPES = {
+}
+
+export const actions = {
+}
---
+++ store/reducers.js
@@ -0,0 +1,6 @@
+export const settings = (state ={}, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
